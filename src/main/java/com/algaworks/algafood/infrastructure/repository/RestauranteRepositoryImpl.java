972695ba9f77ceac942e2600cbd3c4e3c275ceae
Valoir Leite Moura <valoir.moura@certisign.com.br>
2023-01-19 15:05:56 -0300

    Até 5.15. Tornando a consulta com Criteria API com filtros dinâmicos

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -5,11 +5,15 @@ import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -18,31 +22,60 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     @PersistenceContext
     private EntityManager manager;
 
+    //Consulta Dinâmica com Criteria API (SIMPLES)
     @Override
     public List<Restaurante> consultarRestaurantePorNome(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-        var jpql = new StringBuilder();
-        var parametros = new HashMap<String, Object>();
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 
-        jpql.append("from Restaurante where 0 = 0");
+        Root<Restaurante> root = criteria.from(Restaurante.class);
+        var predicates = new ArrayList<Predicate>();
 
         if (StringUtils.hasLength(nome)) {
-            jpql.append("and nome like :nome ");
-            parametros.put("nome", "%" + nome + "%");
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
         }
 
         if (taxaFreteInicial != null) {
-            jpql.append("and taxaFrete >= :taxaInicial ");
-            parametros.put("taxaInicial", taxaFreteInicial);
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
         }
 
         if (taxaFreteFinal != null) {
-            jpql.append("and taxaFrete <= :taxaFinal");
-            parametros.put("taxaFinal", taxaFreteFinal);
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
         }
 
-        TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+        criteria.where(predicates.toArray(new Predicate[0]));
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
         return query.getResultList();
     }
+
+
+// Consulta Dinâmica com JPQL....
+//    @Override
+//    public List<Restaurante> consultarRestaurantePorNome(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+//        var jpql = new StringBuilder();
+//        var parametros = new HashMap<String, Object>();
+//
+//        jpql.append("from Restaurante where 0 = 0");
+//
+//        if (StringUtils.hasLength(nome)) {
+//            jpql.append("and nome like :nome ");
+//            parametros.put("nome", "%" + nome + "%");
+//        }
+//
+//        if (taxaFreteInicial != null) {
+//            jpql.append("and taxaFrete >= :taxaInicial ");
+//            parametros.put("taxaInicial", taxaFreteInicial);
+//        }
+//
+//        if (taxaFreteFinal != null) {
+//            jpql.append("and taxaFrete <= :taxaFinal");
+//            parametros.put("taxaFinal", taxaFreteFinal);
+//        }
+//
+//        TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+//        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+//
+//        return query.getResultList();
+//    }
 }
