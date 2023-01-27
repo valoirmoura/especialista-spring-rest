11e8a1ae12ce56853ddeeccfdd284c0053591544
Valoir Leite Moura <valoir.moura@certisign.com.br>
2023-01-27 11:56:48 -0300

    afterMigrate.sql

---
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,4 @@
+insert ignore into cozinha (id, nome) values (1, 'Tailandesa');
+insert ignore into cozinha (id, nome) values (2, 'Indiana');
+insert ignore into cozinha (id, nome) values (3, 'Argentina');
+insert ignore into cozinha (id, nome) values (4, 'Brasileira');
