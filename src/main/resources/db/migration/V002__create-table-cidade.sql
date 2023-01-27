27b802d87fe6c59968d498ef3297a0cad36e275b
Valoir Leite Moura <valoir.moura@certisign.com.br>
2023-01-27 10:07:15 -0300

    7.7. Evoluindo o banco de dados com novas migrações

---
+++ src/main/resources/db/migration/V002__create-table-cidade.sql
@@ -0,0 +1,7 @@
+create table cidade
+(
+    id          bigint primary key not null auto_increment,
+    nome_cidade varchar(80)        not null,
+    nome_estado varchar(80)        not null
+) engine = InnoDB
+  default charset = utf8;
